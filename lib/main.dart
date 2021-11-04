a8343ae74e72cca13719090cfb56ccfd41ffdeb1
Junaed <junaed1995@gmail.com>
2021-11-05 02:06:23 +0600

    StatefulWidget and Random number added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,13 +11,22 @@ void main() {
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,17 +34,21 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             child: TextButton(
-              child: Image.asset("images/dice1.png"),
+              child: Image.asset("images/dice$leftDiceNumber.png"),
               onPressed: () {
-                debugPrint("Left button clicked");
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
               },
             ),
           ),
           Expanded(
             child: TextButton(
-              child: Image.asset("images/dice2.png"),
+              child: Image.asset("images/dice$rightDiceNumber.png"),
               onPressed: () {
-                debugPrint("Right button called");
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
             ),
           ),
