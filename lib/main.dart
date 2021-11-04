fe89f3681e4f2c3dd452a8ecee87251df0833c37
Junaed <junaed1995@gmail.com>
2021-11-04 21:15:58 +0600

    TextButton added

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset("images/dice1.png"),
-        ),
-        Expanded(
-          child: Image.asset("images/dice1.png"),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset("images/dice1.png"),
+              onPressed: () {
+                debugPrint("Left button clicked");
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset("images/dice2.png"),
+              onPressed: () {
+                debugPrint("Right button called");
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
