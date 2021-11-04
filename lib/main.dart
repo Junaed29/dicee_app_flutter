dae3a88edf1c69b1b3bd3d2391662b4df3d31c7e
Junaed <junaed1995@gmail.com>
2021-11-04 12:06:58 +0600

    Launcher icon and expanded widget added

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +18,15 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset("images/dice1.png"),
+        ),
+        Expanded(
+          child: Image.asset("images/dice1.png"),
+        ),
+      ],
+    );
   }
 }
